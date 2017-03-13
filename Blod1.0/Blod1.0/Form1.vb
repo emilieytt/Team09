f4266077e6139ad175ecf6b08c2d187f6d6f7244
emilieytt <emilie_yttis@hotmail.com>
2017-03-13 14:35:17 +0100

    close form

--- Blod1.0/Blod1.0/Form1.vb
+++ Blod1.0/Blod1.0/Form1.vb
@@ -36,4 +36,8 @@ Public Class Form1
         'Setter bakgrunnsfarge til hvit
         Me.BackColor = Color.White
     End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        Me.Close()
+    End Sub
 End Class
